129419f8912b8a5a9cb25c98e992349f0160278d
kalema1 <kalemar8@gmail.com>
2024-08-14 16:03:25 +0300

    define routes
    - added logic to start the server.
    - added logic to define the get routes.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require("express");
 
 const app = express();
+
+// define routes
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from the server side!");
+});
+
+const port = 3000;
+
+// start up a server
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
