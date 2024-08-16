d227c28cc60a45a26f4530862a5d50a7f51518c1
kalema1 <kalemar8@gmail.com>
2024-08-16 19:39:38 +0300

    handle post api requests
    - added logic to define post route.
    - added logic to add data to the file.
    - added a middleware to use on the request data.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 
 const app = express();
 
+//middle to be use on request data
+app.use(express.json());
+
 // define routes
 /* app.get("/", (req, res) => {
   res
@@ -26,6 +29,25 @@ app.get("/api/v1/tours", (req, res) => {
     results: tours.length,
     data: tours,
   });
+});
+
+// handle post request
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: { tour: newTour },
+      });
+    }
+  );
 });
 
 const port = 3000;
