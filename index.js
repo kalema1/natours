e74fbdc42f2ccfb89a87c06d89b80f483b481868
kalema1 <kalemar8@gmail.com>
2024-08-16 17:27:42 +0300

    define api route
    - added json data in the data folder.
    - read data from the json file in  the data folder.
    - added logic to define api route. used http get method to get data.
    Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
 // define routes
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res
     .status(200)
     .json({ message: "Hello from the server side!", app: "Natours" });
+});
+
+app.post("/", (req, res) => {
+  res.send("You can post to this URL");
+}); */
+
+//read the data from json file before being sent by the api route
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/tours-simple.json`)
+);
+
+// define api routes
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: tours,
+  });
 });
 
 const port = 3000;
