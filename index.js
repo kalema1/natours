35c3777c560b5f7f745746ea17b9461c60c49d70
kalema1 <kalemar8@gmail.com>
2024-08-17 10:52:29 +0300

    handle delete api requests
     - added logic to handle the deletion of item.
     - added logic to handle errors for invalid id.

--- index.js
+++ index.js
@@ -86,6 +86,24 @@ app.patch("/api/v1/tours/:id", (req, res) => {
   });
 });
 
+// handle delete requests
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((element) => element.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
 const port = 3000;
 
 // start up a server
