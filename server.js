79b55a9a9dde35a775c54c9600c8647c9cf3a9e4
kalema1 <kalemar8@gmail.com>
2024-08-19 23:11:52 +0300

    connect to MongoDB
    - added logic in the server.js file. To connect to the MongoDB.
    - added error handling to the logic. To catch errors.

--- server.js
+++ server.js
@@ -1,4 +1,35 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = require("./index");
+
+dotenv.config({ path: "./.env" });
+
+const database = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+let isConnected = false;
+
+async function connectDB() {
+  mongoose.set("strictQuery", true);
+
+  // if connected to the database, dont connect again
+  if (isConnected) {
+    console.log("MongoDB is already Connected...");
+    return;
+  }
+
+  //connect to MongoDB Database
+
+  try {
+    await mongoose.connect(database);
+    isConnected = true;
+    console.log("MongoDB connected...");
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const port = 3000;
 
