655fde0603708c6cbe48af71bb48d422717c5756
kalema1 <kalemar8@gmail.com>
2024-08-21 10:00:43 +0300

    delete documents
    - added logic in the deleteTour function. to delete the selected
      document.
    - added error handling. To catch errors.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -71,19 +71,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((element) => element.id === id);
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "failed to delete data",
+      message: error,
     });
   }
-
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
 };
