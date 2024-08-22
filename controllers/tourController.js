7ace9af9a28434a0f670178a2ec5145cdb0d7645
kalema1 <kalemar8@gmail.com>
2024-08-22 15:01:31 +0300

    refactr the api features
    - created APIFeatures class
    - added methods to the class.
    - used the methods to the API handler.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,11 +1,14 @@
-const fs = require("fs");
 const Tour = require("../models/tourModel");
 
-exports.getAllTours = async (req, res) => {
-  try {
-    //BULD A QUERY
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
     // 1A) Filtering
-    const queryObject = { ...req.query };
+    const queryObject = { ...this.queryString };
     const excludedFields = ["page", "sort", "limit", "fields"];
 
     excludedFields.forEach((element) => delete queryObject[element]);
@@ -17,19 +20,63 @@ exports.getAllTours = async (req, res) => {
       (match) => `$${match}`
     );
 
-    let query = Tour.find(JSON.parse(queryString));
+    this.query = Tour.find(JSON.parse(queryString));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      console.log(sortBy);
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-timestamps");
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+exports.getAllTours = async (req, res) => {
+  try {
+    //BULD A QUERY
+    /* // 1A) Filtering
+    const queryObject = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+
+    excludedFields.forEach((element) => delete queryObject[element]);
+
+    // 1B) Advanced Filtering
+    let queryString = JSON.stringify(queryObject);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    ); */
+
+    // let query = Tour.find(JSON.parse(queryString));
 
     // 2) Sorting
-    if (req.query.sort) {
+    /* if (req.query.sort) {
       const sortBy = req.query.sort.split(",").join(" ");
       console.log(sortBy);
       query = query.sort(sortBy);
     } else {
       query = query.sort("-timestamps");
-    }
+    } */
 
     // 3) Pagination
-    const page = req.query.page * 1 || 1;
+    /* const page = req.query.page * 1 || 1;
     const limit = req.query.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
@@ -41,10 +88,14 @@ exports.getAllTours = async (req, res) => {
       if (skip >= numberOfTours) {
         throw new Error("page does not exist");
       }
-    }
+    } */
 
     // EXECUTE THE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .paginate();
+    const tours = await features.query;
 
     //  SEND RESPONSE
     res.status(200).json({
