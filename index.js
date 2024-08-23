33eb79b725ebaf90719d74f545c68cacc459de20
kalema1 <kalemar8@gmail.com>
2024-08-23 11:50:01 +0300

    create an error middleware
    - created a middleware for error handling at a central place for the
      routes.
    - added logic to the middleware. To catch errors.
    - updated the url error handler.

--- index.js
+++ index.js
@@ -23,9 +23,26 @@ app.use("/api/v1/tours", tourRouter);
 
 // handle error if the url does not exist
 app.all("*", (req, res, next) => {
-  res.status(404).json({
+  /* res.status(404).json({
     status: "fail",
     message: `Cant find ${req.originalUrl} on this server`,
+  }); */
+
+  const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  err.status = "fail";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// handle the route errors in one central middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
