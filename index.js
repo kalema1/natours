c3fd0beb4e8a20753db37c0fea78100441ea945d
kalema1 <kalemar8@gmail.com>
2024-08-23 11:30:46 +0300

    handle errors if the url does not exist
    - added logic to the routes to handle errors if the requested url does
      not exist.

--- index.js
+++ index.js
@@ -21,4 +21,12 @@ app.use((req, res, next) => {
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
 
+// handle error if the url does not exist
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Cant find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
