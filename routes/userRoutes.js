46319eb20c2e9c403a6735bdb21424bd1803c800
kalema1 <kalemar8@gmail.com>
2024-08-23 20:50:53 +0300

    create a sign up user
    - created the authController.js file.
    - create the signup handle function.
    - added logic to create a user.
    - created a signup route.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { createUser, getAllUsers } = require("../controllers/userControler");
+const { signup } = require("../controllers/authController");
 
 const router = express.Router();
+
+router.post("/signup", signup);
 
 router.route("/").get(getAllUsers).post(createUser);
 
